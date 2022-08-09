efb9cf54d399ab32cbdec3801c29c328a8b112b7
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-09 18:51:20 +0200

    update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const connectToWA = () => {
 			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
 			const senderNumber = sender.split('@')[0]
 			const botNumber = conn.user.id.split(':')[0]
-			const pushname = mek.pushName || sender.split('@')[0]
+			const pushname = mek.pushName || 'Sin nombre'
 			const isMe = botNumber.includes(senderNumber)
 			const isOwner = ownerNumber.includes(senderNumber) || isMe
 			
