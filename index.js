cc26da2912db7d47d98a7942fa015c91e790ed1d
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-09 04:34:24 +0200

    Rename main.js to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const {
+	default: makeWASocket,
+	useSingleFileAuthState,
+	DisconnectReason,
+	getContentType
+} = require('@adiwajshing/baileys')
+
+const fs = require('fs')
+const P = require('pino')
+const qrcode = require('qrcode-terminal')
+
+
+
+			
