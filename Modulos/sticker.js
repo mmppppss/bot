3a3f36625de252f4bcebb86bf480ab84989b6ca8
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-09 05:50:54 +0200

    update sticker.js

--- Modulos/sticker.js
+++ Modulos/sticker.js
@@ -1,6 +1,8 @@
 async (mek) =>{
+  
+   const reply = async(teks) => {
 				await conn.sendMessage(from, { text: teks }, { quoted: mek })
-			
+   }
 			
 const pushname = mek.pushName || 'Sin Nombre'
 
