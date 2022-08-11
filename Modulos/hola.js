8adaaea6795c29838160640eed69fb2225b978fe
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 03:06:09 +0200

    update hola.js

--- Modulos/hola.js
+++ Modulos/hola.js
@@ -2,3 +2,24 @@
 exports.menu = `que tal`
 
 exports.time = `Tiempo activo:`+(process.uptime()/60)/60 + `Horas`
+
+exports.shell = 
+console.log("SHELL by: mmppppss");
+const exec=require('child_process').exec;
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    exec(d.toString().trim(), (err, stdout, stderr) => {
+  if (err) {
+    console.error(`error: ${err}`);
+    return;
+  }
+
+  console.log(`[out] ${stdout}`);
+  console.log(`${stderr}`);
+});
+
+
+
+}); 
