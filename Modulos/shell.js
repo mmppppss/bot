52137a26b8102a9755a03423c208af1fd88e2a95
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 18:03:52 +0200

    update shell.js

--- Modulos/shell.js
+++ Modulos/shell.js
@@ -1,1 +1,19 @@
+exports.shell = 
+console.log("SHELL by: mmppppss");
+console.log("CREDITO: Lucky-Cv");
+const exec=require('child_process').exec;
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    exec(d.toString().trim(), (err, stdout, stderr) => {
+  if (err) {
+    console.error(`error: ${err}`);
+    return;
+  }
+
+  console.log(`[out] ${stdout}`);
+  console.log(`${stderr}`);
+});
+}); 
 
