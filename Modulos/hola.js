cf33f6bb5e7af9f6da0bf1c825e2348e1b2a5085
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 04:11:20 +0200

    update hola.js

--- Modulos/hola.js
+++ Modulos/hola.js
@@ -4,26 +4,23 @@ exports.menu = `que tal`
 exports.time = `Tiempo activo:`+(process.uptime()/60)/60 + `Horas`
 
 exports.shell = 
-console.log("SHELL and NODE by: mmppppss");
-console.log("CREDOTOS: Lucky-Vc")
+console.log("SHELL by: mmppppss");
+console.log("CREDITO: Lucky-Cv");
+const exec=require('child_process').exec;
 
 var stdin = process.openStdin();
 
 stdin.addListener("data", function(d) {
-        var query=d.toString();
-                var cmd=query.slice(1);
-        if(query[0]=='>'){
-                a=JSON.stringify(eval(cmd), null, '\t')
-                console.log(a);
-        }
-        if(query[0]=='$'){
-                exec(cmd.trim(), (err, stdout, stderr) => {
-                        if (err) {
-                                console.error(`error: ${err}`);
-                                return;
-                        }
-                console.log(`[out] ${stdout}`);
-                console.log(`${stderr}`);
-                });
-        }
-})
+    exec(d.toString().trim(), (err, stdout, stderr) => {
+  if (err) {
+    console.error(`error: ${err}`);
+    return;
+  }
+
+  console.log(`[out] ${stdout}`);
+  console.log(`${stderr}`);
+});
+
+
+
+}); 
