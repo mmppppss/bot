73b9b26ff21ca6ad02f80b7bc50aa8b0ed500b51
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 18:05:55 +0200

    update menu.js

--- Modulos/menu.js
+++ Modulos/menu.js
@@ -1,26 +1,5 @@
 
 exports.menu = `que tal`
 
-exports.time = `Tiempo activo: ` + Math.round(process.uptime()/60) + ` Minutos`
-
-exports.shell = 
-console.log("SHELL by: mmppppss");
-console.log("CREDITO: Lucky-Cv");
-const exec=require('child_process').exec;
-
-var stdin = process.openStdin();
-
-stdin.addListener("data", function(d) {
-    exec(d.toString().trim(), (err, stdout, stderr) => {
-  if (err) {
-    console.error(`error: ${err}`);
-    return;
-  }
-
-  console.log(`[out] ${stdout}`);
-  console.log(`${stderr}`);
-});
 
 
-
-}); 
