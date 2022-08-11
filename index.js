b917fac8f39f18a8f3952947df79b4c0c915bb7f
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 19:22:27 +0200

    update index.js

--- index.js
+++ index.js
@@ -42,9 +42,6 @@ const connectToWA = () => {
 			console.log('Bot conectado')
 		}
 	})
-	const from = mek.key.remoteJid
-	const img = fs.readFileSync('./lucky.jpg')
-     conn.sendMessage(from, {image: img}, { quoted: '', caption: 'sexo' })
 	
 	conn.ev.on('creds.update', saveState)
 	
@@ -57,7 +54,7 @@ const connectToWA = () => {
 			if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 			const type = getContentType(mek.message)
 			const content = JSON.stringify(mek.message)
-			
+			const from = mek.key.remoteJid
 			
 			const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
 			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
@@ -75,7 +72,9 @@ const connectToWA = () => {
 			const isMe = botNumber.includes(senderNumber)
 			const isOwner = ownerNumber.includes(senderNumber) || isMe
 
-   
+   const img = () => {
+     return'holq'
+   }
 	
 	const more = String.fromCharCode(8206);
 	const readMore = more.repeat(4000);
