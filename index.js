bab6e858a88a573919cf50faa5e00b7d679986e0
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-11 17:46:19 +0200

    update index.js

--- index.js
+++ index.js
@@ -70,7 +70,9 @@ const connectToWA = () => {
 			const isMe = botNumber.includes(senderNumber)
 			const isOwner = ownerNumber.includes(senderNumber) || isMe
 	
+			let msg = cekBahasa(sender);
 			
+		
 			const reply = async(teks) => {
 				await conn.sendMessage(from, { text: teks }, { quoted: mek })
 			}
@@ -89,6 +91,15 @@ case 'img':
   reply(`jajja`)
 break
 
+case 'restart': 
+  case 'reiniciar': 
+    reply(msg.restart)
+try {
+  process.send('reset')
+} catch (e) {
+reply('...')
+}
+  break
 
   break
 
