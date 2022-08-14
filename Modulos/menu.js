442264430e080973d1858aab636db460c86e1d58
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-14 02:34:40 +0200

    update menu.js

--- Modulos/menu.js
+++ Modulos/menu.js
@@ -1,5 +1,13 @@
 
-exports.menu = `que tal`
+exports.menu = var teks = `Hola ${pushname}`
+
+var footer = `jajaja`
+
+var buttons = [
+  {urlButton: {displayText: 'link', url: 'https://hola.com'}},
+	{quickReplyButton: {displayText: '⎙ Lucky-Cv', id: prefix + 'creador'}}
+]
+replyTempImg(teks, footer, buttons, fs.readFileSync('./lucky.jpg'))
 
 
 
