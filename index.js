3571f5ded9d8677ade37cae45d3815713e04f01f
mmppppss <ppozosoliz@gmail.com>
2023-04-21 11:03:57 -0400

    archive.org search

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ const commands = {
     arcsearch:async()=>{
         let jsonData ={};
         let textData="";
-        const search = se
+        const search = body
         const url = `https://archive.org/advancedsearch.php?q=${search}&fl%5B%5D=description&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5&page=1&output=json&callback=callback&save=yes#raw`;
 
         https.get(url, (response) => {
