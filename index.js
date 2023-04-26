24b23411f0c63a6be59382c11bebe47340728a24
mmppppss <ppozosoliz@gmail.com>
2023-04-25 23:52:40 -0400

    archive.org search agian

--- index.js
+++ index.js
@@ -336,35 +336,18 @@ const commands = {
             }
             ytmp4(args[1],from,msg)
         }
-    })
-   /* arcsearch:()=>{
-        let jsonData ={};
-        let textData="";
-        const search = body
-        const url = `https://archive.org/advancedsearch.php?q=${search}&fl%5B%5D=description&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5&page=1&output=json&callback=callback&save=yes#raw`;
-        reply(url)
-        https.get(url, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-              data += chunk;
-            });
-        response.on('end', () => {
-            text=""
-            jsonData=JSON.parse(data.split('"docs":')[1].replace("}})",""))
-    //console.log(jsonData)
-            j=0;
-            for(i of jsonData){
-                textData+=("```[#] "+j+"```\n*Nombre:* "+i.title+"\n*Descripcion:* "+i.description+"\n")
-                j++;
+    }),
+    archives:({args=[]})=>({
+        args,
+        help:"Busca un archivo en archive.org",
+        run(){
+            if(this.args[0]=="-h"){
+                reply(this.help)
+                return this.help
             }
-            conn.sendMessage(from,{ text:"==="+textData})
-            reply(textData)
-        });
-        }).on("error", (err) => {
-            reply("Error: " + err.message);
-            jsonData=err.message
-        });
-    }*/
+         archSearch(body,from,msg)
+        }
+    })
 }
 
 if(isCmd){
@@ -593,6 +576,30 @@ const ytmp4 = async (Link, fromId, quotedMsg) => {
     }
 }
 
-
+const archSearch= async(text, fromId, quotedMsg)=>{
+        let jsonData ={};
+        let textData="";
+        const search = text
+        const url = `https://archive.org/advancedsearch.php?q=${search}&fl%5B%5D=description&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5&page=1&output=json&callback=callback&save=yes#raw`;
+        https.get(url, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+              data += chunk;
+            });
+        response.on('end', () => {
+            text=""
+            jsonData=JSON.parse(data.split('"docs":')[1].replace("}})",""))
+    //console.log(jsonData)
+            j=0;
+            for(i of jsonData){
+                textData+=("```[#] "+j+"```\n*Nombre:* "+i.title+"\n*Descripcion:* "+i.description+"\n")
+                j++;
+            }
+            conn.sendMessage(fromId,{ text:"==="+textData})
+        });
+        }).on("error", (err) => {
+            jsonData=err.message
+        });
+}
 connectToWA()
 sup4Console()
